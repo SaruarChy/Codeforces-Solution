c52549cc3422b0dc1c2bf0470e78621d3a77b0b0
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2019-12-21 20:58:25 +0600

    Math

---
+++ 1269 A. Equation.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    long long int n;
+    cin>>n;
+    if(n==1)
+        cout<<9<<" "<<8<<endl;
+    else
+        cout<<n*4<<" "<<n*3<<endl;
+
+    return 0;
+}
