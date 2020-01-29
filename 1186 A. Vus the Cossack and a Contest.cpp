f25fbd00d43bc6ab81a2c05f7f1aafe7ef8bee90
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2020-01-30 00:50:45 +0600

    Add files via upload

---
+++ 1186 A. Vus the Cossack and a Contest.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,m,k;
+    cin>>n>>m>>k;
+    if(n<=m && n<=k)
+        cout<<"Yes"<<endl;
+    else
+        cout<<"No"<<endl;
+
+    return 0;
+}
