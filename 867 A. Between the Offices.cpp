2374c106fd88ebeecb51e42abca29efb291d240c
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2020-02-07 01:14:02 +0600

    Add files via upload

---
+++ 867 A. Between the Offices.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+//we have to find if i flew more times from San to Francisco
+int main()
+{
+    int n,StoF=0,FtoS=0;
+    string s;
+    cin>>n;
+    cin>>s;
+    for(int i=0; i<n-1; i++)
+    {
+        if(s[i]=='S' && s[i+1]=='F')//if you go San to Francisco
+            StoF++;
+        else if(s[i]=='F' && s[i+1]=='S') //if you go Francisco to San
+            FtoS++;
+    }
+    if(StoF>FtoS)
+        cout<<"YES"<<endl;
+    else
+        cout<<"NO"<<endl;
+
+    return 0;
+}
