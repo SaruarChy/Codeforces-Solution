abe535ec0cb58e403703eca267f01c6a0b979143
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2020-02-06 23:19:41 +0600

    Add files via upload

---
+++ 959 A. Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    if(n%2==0)
+        cout<<"Mahmoud"<<endl;
+    else
+        cout<<"Ehab"<<endl;
+
+    return 0;
+}
