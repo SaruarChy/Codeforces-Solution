013e2bf1928e20f9225967f382bb124bf6911661
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2020-03-05 22:04:02 +0600

    Add files via upload

---
+++ 1093 A. Dice Rolling.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t,n;
+    cin>>t;
+    while(t--){
+        int ans=0;
+        cin>>n;
+//        for(int i=7; i>1; i--){
+//            ans+=n/i;
+//            n=n%i;
+//        }
+        cout<<n/2<<endl;
+    }
+}
