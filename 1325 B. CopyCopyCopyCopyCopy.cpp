19d73192a5458c96d4f4c2a5099e4d09a3333bbc
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2020-03-15 01:09:19 +0600

    Update 1325 B. CopyCopyCopyCopyCopy.cpp

--- 1325 B. CopyCopyCopyCopyCopy.cpp
+++ 1325 B. CopyCopyCopyCopyCopy.cpp
@@ -1,25 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define fastread() (ios_base:: sync_with_stdio(false),cin.tie(NULL));
-
-long long int t,n,a[100001];
 int main()
 {
-    fastread();
+    int t;
     cin>>t;
     while(t--)
     {
-        long int c=0;
-        cin>>n;
-
-        for(long int i=0; i<n; i++){
-            cin>>a[i];
+        int a,b;
+        cin>>a;
+        set<int>s;
+        while(a--)
+        {
+            int b;
+            cin>>b;
+            s.insert(b);
         }
-        sort(a,a+n);
-        for(long int i=0; i<n; i++){
-            if(a[i]!=a[i+1])
-                c++;
-        }
-        cout<<c<<endl;
+        cout<<s.size()<<endl;
     }
+    return 0;
 }
+
