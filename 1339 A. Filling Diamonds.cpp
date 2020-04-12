007b696ae8bf7d1b0e204838c798722fc6a30fa0
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2020-04-12 23:06:09 +0600

    Add files via upload

---
+++ 1339 A. Filling Diamonds.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    long long int n;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        cout<<n<<endl;
+    }
+    return 0;
+}
