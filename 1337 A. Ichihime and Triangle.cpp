ae750b1a71c58eeb27da12f841d3cab44745263c
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2020-04-16 21:24:24 +0600

    Add files via upload

---
+++ 1337 A. Ichihime and Triangle.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    long long int a,b,c,d;
+    cin>>t;
+    while(t--)
+    {
+        cin>>a>>b>>c>>d;
+        cout<<b<<" "<<c<<" "<<c<<endl;
+    }
+    return 0;
+}
+/*
+SIMPLR INPUT
+4
+1 3 5 7
+1 5 5 7
+100000 200000 300000 400000
+1 1 977539810 977539810
+*/
