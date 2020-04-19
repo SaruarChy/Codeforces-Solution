1570f34fbe5edf7cfec2fa4e040d64eaf5080310
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2020-04-19 22:27:45 +0600

    String

---
+++ 281 A. Word Capitalization.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    putchar(toupper(s[0]));
+    for(int i=1; i<s.length(); i++){
+        cout<<s[i];
+    }
+    cout<<"\n";
+    return 0;
+}
+/*
+SIMPLE INPUT
+ApPLe
+*/
