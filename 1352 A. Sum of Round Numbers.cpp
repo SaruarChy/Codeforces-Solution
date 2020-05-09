34d2d8c72f8d632d424617ab02ae9331a39b5a57
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2020-05-09 23:57:57 +0600

    Add files via upload

---
+++ 1352 A. Sum of Round Numbers.cpp
@@ -0,0 +1,42 @@
+//Bismillahir Rahmanir Rahim
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t,n;
+    int aa[100000];
+    cin>>t;
+    while(t--)
+    {
+        int d=0,rem=0;
+        cin>>n;
+        if(n >= 1000){
+            rem=n%1000;
+            aa[d++]=n-rem;
+            n%=1000;
+        }
+        if(n >= 100){
+            rem=n%100;
+            aa[d++]=n-rem;
+            n%=100;
+        }
+        if(n >= 10){
+            rem=n%10;
+            aa[d++]=n-rem;
+            n%=10;
+        }
+        if(n<10 && n>0){
+            aa[d++]=n;
+        }
+        vector<int>va;
+        for(int i=0; i<d; i++){
+            va.push_back(aa[i]);
+        }
+        cout<<va.size()<<endl;
+        for(int i=0; i<va.size(); i++){
+            cout<<va[i]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
