d568201550637fb2176af5bb2f3407f1f5e9a681
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2020-05-24 01:05:56 +0600

    Add files via upload

---
+++ 1081 A. Definite Game.cpp
@@ -0,0 +1,18 @@
+//Bismillahir Rahmanir Rahim
+#include<bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    if(n == 2){
+        cout<<n<<endl;
+    }
+    else
+        cout<<1<<endl;
+
+    return 0;
+}
+
