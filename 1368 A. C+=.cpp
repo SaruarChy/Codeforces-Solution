201fc190fc92b9a8eeaba4e16063b5e5eb97b83e
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2020-06-18 22:24:24 +0600

    Add files via upload

---
+++ 1368 A. C+=.cpp
@@ -0,0 +1,36 @@
+//Bismillahir Rahmanir Rahim
+#include<bits/stdc++.h>
+#define ll              long long
+#define ull             unsigned long long
+#define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+using namespace std;
+int main()
+{
+    fastread();
+    int t;
+    ll a,b,n;
+    cin>>t;
+    while(t--)
+    {
+        cin>>a>>b>>n;
+        ll sum = 0,c = 0;
+        while(sum <= n){
+            if(a > b){
+                b += a;
+                sum = b;
+                c++;
+            }
+            else{
+                a += b;
+                sum = a;
+                c++;
+            }
+            if(sum > n){
+                break;
+            }
+        }
+        cout<<c<<endl;
+    }
+
+    return 0;
+}
