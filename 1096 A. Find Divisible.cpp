cf95b45bb7a2a230c62fdbf5b4e48973102d82c7
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2020-06-26 17:13:43 +0600

    Math

---
+++ 1096 A. Find Divisible.cpp
@@ -0,0 +1,21 @@
+//Bismillahir Rahmanir Rahim
+#include<bits/stdc++.h>
+#define ll              long long
+#define ull             unsigned long long
+#define pb              push_back
+#define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+using namespace std;
+int main()
+{
+    fastread();
+    int t;
+    ll l,r;
+    cin>>t;
+    while(t--){
+        cin>>l>>r;
+
+        cout<<l<<" "<<l*2<<endl;
+    }
+
+    return 0;
+}
