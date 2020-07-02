4e10e205491fd7db846c2c51ef266cd6f9f46d2a
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2020-07-02 21:52:20 +0600

    Math

---
+++ 1342 A. Road To Zero.cpp
@@ -0,0 +1,31 @@
+//Bismillahir Rahmanir Rahim
+#include<bits/stdc++.h>
+#define ll              long long
+#define ull             unsigned long long
+#define pb              push_back
+#define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+using namespace std;
+int main()
+{
+    fastread();
+    int t;
+    ll x,y,a,b;
+    cin>>t;
+    while(t--){
+        cin>>x>>y>>a>>b;
+
+        if(x > y){
+            swap(x,y);
+        }
+        ll ans1 = 0, ans2 = 0;
+        ans1 = x*a + y*a;
+        ll z = y - x;
+        ans2 = z*a + x * b;
+
+        cout<<min(ans1,ans2)<<endl;
+
+    }
+
+    return 0;
+}
+
