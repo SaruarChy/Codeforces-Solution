d1b284d3e15c5a3c861d808985ca62da845b298c
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2020-07-12 01:30:42 +0600

    Add files via upload

---
+++ 1372 A. Omkar and Completion.cpp
@@ -0,0 +1,23 @@
+//Bismillahir Rahmanir Rahim
+#include<bits/stdc++.h>
+#define ll              long long
+#define ull             unsigned long long
+#define pb              push_back
+#define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+using namespace std;
+int main()
+{
+    fastread();
+    int t,n;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        for(int i=0; i<n; i++){
+            cout<<1<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
+
