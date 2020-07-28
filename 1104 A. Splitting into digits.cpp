582e097ff5e9e048de80ff199ab9c4b71609bad8
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2020-07-29 03:16:10 +0600

    Math

---
+++ 1104 A. Splitting into digits.cpp
@@ -0,0 +1,21 @@
+//Bismillahir Rahmanir Rahim
+#include<bits/stdc++.h>
+#define ll              long long
+#define ull             unsigned long long
+#define pb              push_back
+#define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+using namespace std;
+int main()
+{
+    fastread();
+    int n;
+    cin>>n;
+
+    cout<<n<<endl;
+    for(int i=0; i<n; i++){
+        cout<<1<<" ";
+    }cout<<endl;
+
+    return 0;
+}
+
