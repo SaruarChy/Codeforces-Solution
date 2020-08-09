ca34467d9820e2de926619a438e1786631ae468c
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2020-08-09 23:53:27 +0600

    Add files via upload

---
+++ 1391 A. Suborrays.cpp
@@ -0,0 +1,24 @@
+//Bismillahir Rahmanir Rahim
+#include<bits/stdc++.h>
+#define ll              long long
+#define ull             unsigned long long
+#define pb              push_back
+#define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+using namespace std;
+int main()
+{
+    fastread();
+    int t,n;
+    cin>>t;
+    while(t--){
+        int a[101];
+        cin>>n;
+        int j = n;
+        for(int i=1; i<=n; i++){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
