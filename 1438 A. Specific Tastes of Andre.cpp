4da2d8855c3af50cdc0bdb9541f9ae643cc1b2c8
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2020-11-13 23:57:05 +0600

    Add files via upload

---
+++ 1438 A. Specific Tastes of Andre.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define ll              long long
+#define ull             unsigned long long
+#define pb              push_back
+#define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+using namespace std;
+int main()
+{
+    fastread();
+
+    //freopen("input.txt","r", stdin);
+
+    ll t,n;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        for(int i=0; i<n; i++){
+            cout<<n<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
