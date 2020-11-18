5673c02bf339a2b3bae4f3bce26ef94b4e000871
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2020-11-18 21:50:15 +0600

    Math

---
+++ 805 A. Fake NP.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define ll              long long
+#define ull             unsigned long long
+#define pb              push_back
+#define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+using namespace std;
+int main()
+{
+    fastread();
+    //freopen("input.txt","r", stdin);
+
+    ll l,r;
+    cin>>l>>r;
+
+    if(l == r){
+        cout<<l<<endl;
+    }
+    else{
+        cout<<2<<endl;
+    }
+
+    return 0;
+}
