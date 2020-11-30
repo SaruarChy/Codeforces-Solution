0fc65896aad0614af9d64731808680f5fd72fd90
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2020-11-30 23:31:06 +0600

    Math

---
+++ 1455 A. Strange Functions.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define ll              long long
+#define ull             unsigned long long
+#define pb              push_back
+#define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+using namespace std;
+int main()
+{
+    fastread();
+    //freopen("input.txt","r", stdin);
+    ll t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+
+        cout<<s.length()<<endl;
+    }
+
+    return 0;
+}
