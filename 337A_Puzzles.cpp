2ed25a1dbcce205b818da8c93e54da3373283508
Aman Chandra <112195895+amanchandra395@users.noreply.github.com>
2023-02-06 11:58:49 +0530

    Greedy

---
+++ 337A_Puzzles.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n, m, f[1000];
+    cin >> n >> m;
+    for (int i = 0; i < m; ++i)
+    {
+        cin >> f[i];
+    }
+    sort(f, f + m);
+    int least = f[n-1] - f[0];
+    for (int i = 1; i <= m - n; ++i)
+    {
+        if (f[i+n-1] - f[i] < least)
+        {
+            least = f[i+n-1] - f[i];
+        }
+    }
+    cout << least << endl;
+    return 0;
+}
